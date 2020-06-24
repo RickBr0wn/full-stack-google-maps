65017e2b995552909766b92e5156fb8b2fa7367b
Rick Brown <ricky.brown.00@gmail.com>
2020-06-24 21:39:13 +0100

    adds markers

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useCallback, useRef } from 'react'
 import {
   GoogleMap,
   useLoadScript,
@@ -32,6 +32,21 @@ const Map = () => {
     libraries,
   })
 
+  const [markers, setMarkers] = useState([])
+  const [selected, setSelected] = useState(null)
+
+  const onMapClick = useCallback(
+    e =>
+      setMarkers(current => [
+        ...current,
+        { lat: e.latLng.lat(), lng: e.latLng.lng(), time: new Date() },
+      ]),
+    []
+  )
+
+  const mapRef = useRef()
+  const onMapLoad = useCallback(map => (mapRef.current = map), [])
+
   if (loadError) return 'Error loading Google Maps 🗺'
   if (!isLoaded) return 'Loading Google Maps data 🗺'
 
@@ -48,7 +63,32 @@ const Map = () => {
         zoom={15}
         center={center}
         options={options}
-      />
+        onClick={onMapClick}
+        onLoad={onMapLoad}>
+        {markers.map(marker => (
+          <Marker
+            key={marker.time.toISOString()}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: '/bear.svg',
+              scaledSize: new window.google.maps.Size(30, 30),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => setSelected(marker)}
+          />
+        ))}
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => setSelected(null)}>
+            <div>
+              <h2>Bear Spotted!</h2>
+              <p>Spotted at {formatRelative(selected.time, new Date())}</p>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   )
 }
